bd88b2e7c25b2a5ea0805968540d0fd195042d06
qudusdev <qudusdev@gmail.com>
2018-02-10 00:50:37 +0100

    Added event listeners in the JS file. Styled h1 and button

---
+++ index.js
@@ -0,0 +1,21 @@
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+left.addEventListener('mouseenter', function () {
+  container.classList.add('hover-left')
+});
+
+left.addEventListener('mouseleave', function () {
+  container.classList.remove('hover-left')
+});
+
+rightt.addEventListener('mouseenter', function () {
+  container.classList.add('hover-right')
+});
+
+right.addEventListener('mouseleave', function () {
+  container.classList.remove('hover-right')
+});
+
+
